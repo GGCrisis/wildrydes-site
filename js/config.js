4921e3c2e616bb62cd558c7781ef9c7bff1c2de8
GGCrisis <99818968+GGCrisis@users.noreply.github.com>
2025-11-23 17:01:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_Edkqu0fP3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '55m8igrnna6lqen5373tnrm2fh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_X7EPaBcX0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38idsakiigou81v2r4mtlt4fm9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
